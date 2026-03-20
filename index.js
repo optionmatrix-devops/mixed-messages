7601021d597523861b32f1d2565be96e5bfe4298
optionmatrix2002 <navin@optionmatrix.com>
2026-03-20 17:52:19 +0530

    add function to generate message

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-console.log("Thank you");
-const name = "Sai Shwejan";
-console.log(name);
+function alienForecast() {
+  const forecasts = [
+    "Today is a great day to be an alien!",
+    "Beware of the human invasion.",
+    "The stars are aligning in your favor.",
+    "You may encounter some strange phenomena today.",
+    "Your alien powers will be at their peak.",
+    "A cosmic event is on the horizon.",
+    "You will make contact with a new species soon.",
+    "Your spaceship will need repairs, but don't worry, you'll find the parts you need.",
+    "The universe has a surprise in store for you today."
+  ];
+  return forecasts[Math.floor(Math.random() * forecasts.length)];
+
+}
+
+console.log(alienForecast());
